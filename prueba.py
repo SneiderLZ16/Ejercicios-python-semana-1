d339c85560c2eabcb558b48d2b19e9fdc7658ec1
SneiderLZ16 <sneiderlondono0216@gmail.com>
2025-05-12 07:47:16 -0500

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,104 @@
+
+
+inventario = {}
+
+
+def añadir_producto():
+    producto = input("Product name: ").strip()
+    if producto in inventario:
+        print("This product already exists in the catalog.\n")
+        return
+
+    precio = float(input("Price: ").strip())
+    
+
+    try:
+        cantidad = int(input("Amount of product: "))
+        if cantidad <= 0:
+            raise ValueError
+    except ValueError:
+        print("Invalid number")
+        return
+
+    inventario[producto] = {
+        "precio": precio,
+        "cantidad_disponible": cantidad,
+    }
+
+    print(f"product '{producto}' added successfully.\n")
+    
+    
+def buscar_p():
+    producto = input("Enter product to search: ").strip()
+    bs = inventario.get(producto)
+    if bs:
+        print(f"\nProduct: {producto}")
+        print(f"Price: {bs['precio']}")
+        print(f"Products availables: {bs['cantidad_disponible']}")
+    else:
+        print("product not found in the catalog.\n")
+
+def editar_p():
+    precio = float(input("Product price to update: "))
+    for precio in inventario:
+        if inventario[precio] == precio:
+            nuevo_precio = int(input(f"new price: "))
+            
+            if nuevo_precio:
+                inventario[precio] = nuevo_precio
+                
+            print("Price updated.\n")
+            return
+    print(f"price '{precio}' didn't found.\n")
+    
+    
+
+
+def eliminar_prod():
+    producto = input("Enter the product to delete: ").strip()
+    bs = inventario.get(producto)
+    if bs:
+        if bs['cantidad_disponible']>0:
+            del inventario[producto]
+            print(f"product '{producto}' removed from the catalog.\n")
+    else:
+        print("product not found in the catalog.\n")
+        
+        
+
+def calc_to():
+    print
+    
+
+
+def mostrar_menu():
+    print("=== inventory processing ===")
+    print("1. Add product")
+    print("2. Search product")
+    print("3. update product")
+    print("4. Delete product")
+    print("5. Calculate full inventory")
+    print("6. Exit")
+
+
+
+
+
+while True:
+    mostrar_menu()
+    opcion = int(input("Select an option (1-6): ").strip())
+    print()
+    
+    if opcion == 1:
+        añadir_producto()
+    elif opcion==2:
+        buscar_p()
+    elif opcion==3:
+        editar_p()
+    elif opcion==4:
+        eliminar_prod()
+    elif opcion==5:
+        calc_to()
+    elif opcion==6:
+        False
+        
